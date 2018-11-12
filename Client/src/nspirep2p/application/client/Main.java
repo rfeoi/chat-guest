c4ab9772bd933f9da62c9b539210453cc50f99db
robmroi03 <robmori@web.de>
2018-11-12 09:32:56 +0100

    Added the basics for the login screen

--- Client/src/nspirep2p/application/client/Main.java
+++ Client/src/nspirep2p/application/client/Main.java
@@ -5,17 +5,31 @@ package nspirep2p.application.client;
  * Starts client
  * Created by strifel on 05.11.2018.
  */
+
+import nspirep2p.application.client.connection.ConnectionHandler;
+
+/**
+ * TODO:
+ * GUI
+ * Communication between server and TI
+ * Tests(?)
+ */
 public class Main {
+    private UserInterface userInterface;
+    public ConnectionHandler connectionHandler;
+    static Main mainClass;
+
     public static void main(String[] args){
-        Main main = new Main();
-        main.start();
+        mainClass = new Main();
+        mainClass.start();
     }
 
     private Main(){
-
+        userInterface = new UserInterface();
+        connectionHandler = new ConnectionHandler();
     }
 
     private void start() {
-
+        userInterface.start();
     }
 }
---
+++ Client/src/nspirep2p/application/client/connection/ConnectionHandler.java
@@ -0,0 +1,12 @@
+package nspirep2p.application.client.connection;
+
+/**
+ * Created by robmroi03 on 12.11.2018.
+ */
+public class ConnectionHandler {
+    public void connect(String ip, String username){
+        System.out.println("IP: " + ip);
+        System.out.println("username: " + username);
+        //TODO sockets and some fun
+    }
+}
