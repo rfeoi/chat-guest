cab5612c16808f1e5fa1f53252e804ed6606862b
strifel <felixletsplay@outlook.de>
2018-11-26 09:35:52 +0100

    Added gson to dependencies Reading permissions from database

--- Server/src/main/java/nspirep2p/application/server/database/PermissionManagment.java
+++ Server/src/main/java/nspirep2p/application/server/database/PermissionManagment.java
@@ -1,9 +1,10 @@
 package nspirep2p.application.server.database;
 
+import com.google.gson.Gson;
 import nspirep2p.application.server.connection.Client;
 import org.tmatesoft.sqljet.core.SqlJetException;
 import org.tmatesoft.sqljet.core.SqlJetTransactionMode;
-import org.tmatesoft.sqljet.core.internal.table.SqlJetTable;
+import org.tmatesoft.sqljet.core.table.ISqlJetCursor;
 import org.tmatesoft.sqljet.core.table.ISqlJetTable;
 import org.tmatesoft.sqljet.core.table.SqlJetDb;
 
@@ -14,6 +15,7 @@ public class PermissionManagment {
     private HashMap<String, String[]> permissions;
     private HashMap<String, String> keys;
     private SqlJetDb database;
+    private Gson gson = new Gson();
     public PermissionManagment(DatabaseManaging database){
         //Initialize variables
         permissions = new HashMap<String, String[]>();
@@ -31,10 +33,33 @@ public class PermissionManagment {
     private void reLoadDatabase() throws SqlJetException {
         database.beginTransaction(SqlJetTransactionMode.READ_ONLY);
         ISqlJetTable table = database.getTable("roles");
+        ISqlJetCursor cursor = table.order(table.getPrimaryKeyIndexName());
+        try {
+            if (!cursor.eof()) {
+                do {
+                    String role = cursor.getString("name");
+                    String[] permissions = gson.fromJson(cursor.getString("permissions"), String[].class);
+                    this.permissions.put(role, permissions);
+                    keys.put(role, cursor.getString("key"));
+                } while (cursor.next());
+            }
+        } finally {
+            cursor.close();
+        }
+
     }
 
     public void clientHasPermission(Client client, Permission permission){
+        roleHasPermission(client.getRole(), permission);
+    }
 
+    private boolean roleHasPermission(String role, Permission permission) {
+        for (String perm : permissions.get(role)) {
+            if (perm.equals(permission.toString())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
